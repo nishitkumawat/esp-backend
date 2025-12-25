bc5855c4da243acff23d97b2c0b2c46436340262
nishitkumawat <nishit1060@gmail.com>
2025-12-25 22:51:52 +0530

    Admin Panel req

--- backend/urls.py
+++ backend/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include   
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('ezrun_admin/', admin.site.urls),
     path('iot/', include('iot.urls')),
 ]
