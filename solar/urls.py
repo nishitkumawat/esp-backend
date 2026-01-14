d7ba1c1ba2b5d236609aa50ae0d90603db22a8de
nishitkumawat <nishit1060@gmail.com>
2026-01-14 20:54:24 +0530

    live server deploy on 14-01-20206 8

--- solar/urls.py
+++ solar/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('stats', views.get_solar_stats, name='solar_stats'),
     # path('ping', views.ping_location, name='solar_ping'),
     # path('device/complete-setup', views.complete_setup, name='complete_setup'),
-    path('location-ping', views.save_device_location, name='save_device_location'),
+    path('location-ping/', views.save_device_location, name='save_device_location'),
 ]
